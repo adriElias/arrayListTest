dfb086358eb4e6e3e63435004879b6e53bf2d648
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-25 19:15:45 +0100

    feat: create classes for testing

---
+++ src/main/java/org/arrayList/Phone.java
@@ -0,0 +1,17 @@
+package org.arrayList;
+
+public class Phone {
+    private String brand;
+
+    public Phone(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
