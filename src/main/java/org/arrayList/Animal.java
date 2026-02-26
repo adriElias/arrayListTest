0c03ea8622441bb8d463c43deb1c946b581b6ee0
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-26 10:50:00 +0100

    feat: exercise 7-> Create an optional object. Write the correct statement to verify that it is indeed constructed

--- src/main/java/org/arrayList/Animal.java
+++ src/main/java/org/arrayList/Animal.java
@@ -1,5 +1,7 @@
 package org.arrayList;
 
+import java.util.Objects;
+
 public class Animal {
     private String name;
     private final String[] abilities = {"run", "jump", "eat"};
@@ -18,5 +20,17 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(getName(), animal.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getName());
     }
 }
